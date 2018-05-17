2c2c8c25ceaf3e9441219ca7f0e5535e05674855
proninyaroslav <yaroslav.pronin1@gmail.com>
2018-05-17 21:02:07 +0300

    First release

---
+++ spam-to-blacklist/readme.txt
@@ -0,0 +1,31 @@
+=== Spam to blacklist ===
+Contributors: proninyaroslav
+Donate link: https://github.com/proninyaroslav/spam-to-blacklist
+Tags: comments, spam, blacklist, ban, ip
+Requires at least: 4.9
+Tested up to: 4.9.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Adds IP from comment that marked as spam to standard WordPress blacklist.
+
+== Description ==
+
+Adds IP from comment that marked as spam to standard WordPress blacklist. Comments already marked as spam are not added to the list.
+
+== Installation ==
+
+1. Install this plugin either via the WordPress.org plugin directory, or by uploading the files to your server
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. That's it. You're ready to go!
+
+== Changelog ==
+
+= 1.0 =
+
+* First release.
+
+== Upgrade Notice ==
+
+No Upgrade Notice. This is the first release.
---
+++ spam-to-blacklist/spam-to-blacklist.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Adds IP from comment that marked as spam to standard WordPress blacklist. Comments already marked as spam are not added to the list.
+ *
+ * @link              https://github.com/proninyaroslav/spam-to-blacklist
+ * @since             1.0
+ * @package           WordPress
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Spam to blacklist
+ * Plugin URI:        https://github.com/proninyaroslav/spam-to-blacklist
+ * Description:       Adds IP from comment that marked as spam to standard WordPress blacklist. Comments already marked as spam are not added to the list.
+ * Version:           1.0
+ * Author:            Yaroslav Pronin
+ * Author URI:        https://proninyaroslav.ru
+ * License:           GPLv2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+/*
+ * Copyright (C) 2018 Yaroslav Pronin <proninyaroslav@mail.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+add_action('spam_comment', 'ban', 10, 2);
+add_action('unspam_comment', 'unban', 10, 2);
+
+function ban($comment_id, $comment)
+{
+	$blacklist = get_option('blacklist_keys', '');
+	$blacklist .= "\n" . $comment->comment_author_IP;
+	update_option('blacklist_keys', $blacklist);
+}
+
+function unban($comment_id, $comment)
+{
+	$blacklist = get_option('blacklist_keys', '');
+	$blacklist = str_replace($comment->comment_author_IP, '', $blacklist);
+	update_option('blacklist_keys', $blacklist);
+}
